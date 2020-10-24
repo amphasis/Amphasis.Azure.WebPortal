2f3102199c33dd9a2d38ab2e4795bba23a3567c2
Хоботов Лев <hl-studio@mail.ru>
2020-10-24 22:11:04 +0300

    Add SimaLandImageSize enum

---
+++ Leff.Azure.WebApplication/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Leff.Azure.WebApplication.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetEnumMemberValue<T>(this T value) where T : struct, IConvertible
+        {
+            return typeof(T)
+                .GetTypeInfo()
+                .DeclaredMembers
+                .SingleOrDefault(x => x.Name == value.ToString())
+                ?.GetCustomAttribute<EnumMemberAttribute>(false)
+                ?.Value;
+        }
+    }
+}
---
+++ Leff.Azure.WebApplication/Models/Enums/SimaLandImageSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace Leff.Azure.WebApplication.Models.Enums
+{
+    public enum SimaLandImageSize
+    {
+        [EnumMember(Value = "140.jpg")]
+        Quad140,
+
+        [EnumMember(Value = "280.jpg")]
+        Quad280,
+
+        [EnumMember(Value = "400.jpg")]
+        Quad400,
+
+        [EnumMember(Value = "700-nw.jpg")]
+        Quad700,
+
+        [EnumMember(Value = "1600.jpg")]
+        Quad1600Watermark
+    }
+}
