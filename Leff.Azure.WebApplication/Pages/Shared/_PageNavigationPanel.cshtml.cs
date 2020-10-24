ac4b1ab1c153cceacb0d89b3c2a44841fefbcc2b
Хоботов Лев <hl-studio@mail.ru>
2020-10-25 00:17:35 +0300

    Implement page navigation partial view

---
+++ Leff.Azure.WebApplication/Pages/Shared/_PageNavigationPanel.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Leff.Azure.WebApplication.Pages.Shared
+{
+    public class PageNavigationPanelModel
+    {
+        private int _currentPage;
+        private int _firstPage;
+        private int _navigationSpread;
+
+        public PageNavigationPanelModel(int currentPage)
+        {
+            _firstPage = 1;
+            _navigationSpread = 5;
+            CurrentPage = currentPage;
+        }
+
+        public int CurrentPage
+        {
+            get => _currentPage;
+            set
+            {
+                if (value < FirstPage) throw new ArgumentOutOfRangeException();
+                _currentPage = value;
+            }
+        }
+
+        public int FirstPage
+        {
+            get => _firstPage;
+            set
+            {
+                if (value < 0 || value > CurrentPage) throw new ArgumentOutOfRangeException();
+                _firstPage = value;
+            }
+        }
+
+        public int NavigationSpread
+        {
+            get => _navigationSpread;
+            set
+            {
+                if (value < 1) throw new ArgumentOutOfRangeException();
+                _navigationSpread = value;
+            }
+        }
+    }
+}
