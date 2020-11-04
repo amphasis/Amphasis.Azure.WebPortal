95d6295d5eaf744637f3dcfa19396052286b64f7
Хоботов Лев <hl-studio@mail.ru>
2020-11-04 12:01:56 +0300

    Add authentication

--- Amphasis.Azure.WebPortal/Startup.cs
+++ Amphasis.Azure.WebPortal/Startup.cs
@@ -1,13 +1,22 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Amphasis.Azure.WebPortal.Models;
 using Amphasis.Azure.WebPortal.SimaLand.Models;
 using Amphasis.Azure.WebPortal.SimaLand.Services;
 using Amphasis.Azure.WebPortal.Yandex.Models;
 using Amphasis.SimaLand;
+using AspNet.Security.OAuth.MailRu;
+using AspNet.Security.OAuth.Vkontakte;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace Amphasis.Azure.WebPortal
 {
@@ -41,6 +50,59 @@ namespace Amphasis.Azure.WebPortal
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services
+                .AddAuthentication(options => options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/SignIn";
+                    options.LogoutPath = "/SignOut";
+                })
+                .AddMailRu(options =>
+                {
+                    _configuration.GetSection("MailRu").Bind(options);
+
+                    options.Events.OnCreatingTicket += async context =>
+                    {
+                        var identity = context.Identity;
+                        var imageClaim = identity.FindFirst(MailRuAuthenticationConstants.Claims.ImageUrl);
+                        identity.RemoveClaim(imageClaim);
+                        imageClaim = new Claim(CustomClaims.UserImageUrl, imageClaim.Value, imageClaim.ValueType);
+                        identity.AddClaim(imageClaim);
+                    };
+                })
+                .AddVkontakte(options =>
+                {
+                    _configuration.GetSection("VK").Bind(options);
+
+                    options.Events.OnCreatingTicket += async context =>
+                    {
+                        var identity = context.Identity;
+                        var imageClaim = identity.FindFirst(VkontakteAuthenticationConstants.Claims.PhotoUrl);
+                        identity.RemoveClaim(imageClaim);
+                        imageClaim = new Claim(CustomClaims.UserImageUrl, imageClaim.Value, imageClaim.ValueType);
+                        identity.AddClaim(imageClaim);
+                    };
+                })
+                .AddYandex(options =>
+                {
+                    _configuration.GetSection("Yandex").Bind(options);
+
+                    options.Events.OnCreatingTicket += async context =>
+                    {
+                        var uri = new Uri("https://login.yandex.ru/info");
+                        var authorization = new AuthenticationHeaderValue("OAuth", context.AccessToken);
+                        using var httpClient = new HttpClient();
+                        httpClient.DefaultRequestHeaders.Authorization = authorization;
+                        using var httpResponseMessage = await httpClient.GetAsync(uri);
+                        httpResponseMessage.EnsureSuccessStatusCode();
+                        var contentString = await httpResponseMessage.Content.ReadAsStringAsync();
+                        var userInfo = JsonConvert.DeserializeObject<YandexUserInfo>(contentString);
+                        var imageUrl = $"https://avatars.yandex.net/get-yapic/{userInfo.DefaultAvatarId}/islands-200";
+                        var imageClaim = new Claim(CustomClaims.UserImageUrl, imageUrl, ClaimValueTypes.String);
+                        context.Identity.AddClaim(imageClaim);
+                    };
+                });
+
             var razorPagesBuilder = services.AddRazorPages();
             services.AddControllers();
 
@@ -69,6 +131,10 @@ namespace Amphasis.Azure.WebPortal
             applicationBuilder.UseCookiePolicy();
 
             applicationBuilder.UseRouting();
+
+            applicationBuilder.UseAuthentication();
+            applicationBuilder.UseAuthorization();
+
             applicationBuilder.UseEndpoints(builder =>
             {
                 builder.MapRazorPages();
